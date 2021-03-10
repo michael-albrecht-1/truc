912491df7e69577fd35a6b5c9c836cc6e7eb4423
michael-albrecht-1 <albrecht.michael@hotmail.fr>
2021-03-10 21:18:42 +0100

    integrate scss and navbar html

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import firebase from "../../utils/firebaseConfig";
 
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+    const [areProfilActionsDisplayed, setAreProfilActionsDisplayed] = useState(false);
     return (
-        <nav className="navbar container">
-            <h1>Le truc que j'préfère !</h1>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/beers">Bières</Link>
-                </li>
-                <li>
-                    <div
-                        className="logout"
-                        onClick={() => firebase.auth().signOut()}
-                    >Déconnecter  {firebase.auth().currentUser.displayName}</div>
-                </li>
-            </ul>
-        </nav>
+        <header className="navbar">
+            <div className="header">
+                <h1>Le truc que j'préfère !</h1>
+                <ul className="profil">
+                    {
+                       (!areProfilActionsDisplayed) && (
+                        <li onClick={() => setAreProfilActionsDisplayed(true)}>Profil</li>
+                    ) 
+                    }
+                    { (areProfilActionsDisplayed) && (
+                            <>
+                                <li onClick={() => firebase.auth().signOut()}>Déconnecter  {firebase.auth().currentUser.displayName}</li>
+                                <li onClick={() => setAreProfilActionsDisplayed(false)}>Fermer</li>
+                            </>
+                        )
+                    }
+                </ul>
+            </div>
+            
+            <nav className="links">
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/beers">Bières</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
