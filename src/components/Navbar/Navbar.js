ab1b819c5783aebd753a065b216c05b90537e9d6
michael-albrecht-1 <albrecht.michael@hotmail.fr>
2021-03-11 11:37:20 +0100

    navbar v1 ok

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,33 @@
 import React, { useState } from 'react';
-
 import firebase from "../../utils/firebaseConfig";
-
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
     const [areProfilActionsDisplayed, setAreProfilActionsDisplayed] = useState(false);
+
+    const date = new Date();
+    const months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"];
+
     return (
         <header className="navbar">
             <div className="header">
-                <h1>Le truc que j'préfère !</h1>
+                <div className="logo">
+                    <Link to="/"><img src="../../images/moon2.svg" alt="logo" /></Link>
+                </div>
+                <div className="title">
+                    <Link to="/">
+                        <div className="date">
+                            {
+                                'Le '
+                                +date.getDate() 
+                                + ' ' 
+                                + months[date.getMonth()] 
+                                + ' ' +date.getFullYear()
+                            }
+                        </div>
+                        <h1>Le truc que j'préfère !</h1>
+                    </Link>
+                </div>
                 <ul className="profil">
                     {
                        (!areProfilActionsDisplayed) && (
