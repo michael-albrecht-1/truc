9f268a33fb8aec9bd1c38f5e08c9a0ba68b65d8f
michael-albrecht-1 <albrecht.michael@hotmail.fr>
2021-03-16 21:05:33 +0100

    fix index.tsx TS errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 import "./styles/styles.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
