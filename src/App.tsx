5b14cab78518641178887f72c90c23a5049bc9f9
michael-albrecht-1 <albrecht.michael@hotmail.fr>
2021-03-17 17:50:22 +0100

    fix TS errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import * as React from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import firebase from "./utils/firebaseConfig";
