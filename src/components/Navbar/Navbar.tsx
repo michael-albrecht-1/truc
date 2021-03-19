5aaf84c3c804cb710593fdc5ee63d657d6c3e9ea
michael-albrecht-1 <albrecht.michael@hotmail.fr>
2021-03-19 10:24:27 +0100

    remove useless import

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { StyledList, StyledListItem, StyledListItemIcon } from "./Navbar.styled";
-import { StyledI } from "../../sharedStyle/styles"
  
 const Navbar = () => {
   return (
